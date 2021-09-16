ca36ead4f90e05d740208ed174f188664552a34e
cjlee38 <gptpem38@gmail.com>
2021-09-16 17:18:38 +0900

    commit to pull

--- file.py
+++ file.py
@@ -1,12 +1,36 @@
 import os
 from abc import *
 
-class FileInfo(metaclass = ABCMeta) :
+class Component(ABC) :
+
+    @property
+    def parent(self) :
+        return self._parent
+    
+    @parent.setter
+    def parent(self, parent) :
+        self._parent = parent
+    
+    def add(self, component) -> None :
+        pass
+    
+    def remove(self, component) -> None :
+        pass
+
+    def is_composite(self) -> bool :
+        return False
+
+    @abstractmethod
+    def operation(self) :
+        pass
+
+class FileInfo(ABC) :
     def __init__(self, name, path) :
         self.name = name
         self.abs_path, self.rel_path = self.parse_path(path)
 
     def parse_path(self, path) :
+        # todo : fix this lol
         pass
 
     def get_name(self) :
@@ -42,3 +66,32 @@ class ImageInfo(FileInfo) :
 
     def get_count(self) :
         return self.count
+
+
+# ------------------ #
+    def extract_image_links(self, fileinfo) :
+        def parse_link(sentence) :
+            split = RegexHandler.IMAGE_LINK.findall(sentence)[0]
+            ret = {
+                'alt' : split[0], 
+                'src' : split[1]
+            }
+            return ret
+
+        fp = open(fileinfo['directory'] + '/' + fileinfo['filename'], 'r')
+        links = []
+        while True :
+            sentence = fp.readline()
+            if not sentence : break
+            if RegexHandler.is_pattern_match(sentence, RegexHandler.IMAGE_LINK) :
+                links.append(parse_link(sentence))
+        fp.close()
+        return links
+
+    def is_alive(self, md, link) :
+        image = md['directory'] + '/' + link['src']
+        # print(image)
+        return os.path.isfile(image)
+        # print(os.path.isfile(image))
+        # print('md = ', md)
+        # print('link = ', link)
