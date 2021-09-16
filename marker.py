db1e1e738e293acc8f405dc6276abece86f8d4cd
cjlee38 <gptpem38@gmail.com>
2021-09-16 09:31:31 +0900

    ADD : draft for marker

--- marker.py
+++ marker.py
@@ -10,60 +10,40 @@ class AbstractMarker(metaclass = ABCMeta) :
         pass
 
 class Marker(AbstractMarker) :
-    def __init__(self) :
+    def __init__(self, root) :
+        self.root = root
+        self.image_files = self.collect_fileinfo(RegexHandler.IMAGE_FILE, root = root)
         pass
+
+    def collect_fileinfo(self, pattern, root) :
+        fileinfo = []
+        for root, dir, files in os.walk(root) :
+            for file in files :
+                if RegexHandler.is_pattern_match(file, pattern) :
+                    fileinfo.append(FileInfo(name = file, path = root))
+        return fileinfo
 
     def mark(self) :
         '''
         returns a list of image files not linked to markdown text
         '''
-        markdown_files = self.collect_fileinfo(RegexHandler.MARKDOWN_FILE)
-        image_files = self.collect_fileinfo(RegexHandler.IMAGE_FILE)
-        print(image_files)
-
-        for md in markdown_files :
-            links = self.extract_image_links(md)
+        for markdown_info in self.traverse() :
+            links = markdown_info.extract_links()
             for link in links :
-                self.count(image_files, link)
-                self.is_alive(md, link) # todo : migrate to health checker
-            
+                if link.is_alive() :
+                    self.count(link)
+    
+    def traverse(self) :
+        for root, dir, files in os.walk(self.root) :
+            for file in files :
+                if RegexHandler.is_pattern_match(file, RegexHandler.MARKDOWN_FILE) :
+                    yield MarkdownInfo(name = file, path = root)
+
+
     def count(self, image_files, link) :
         link['src']
-    def collect_fileinfo(self, pattern) :
-        fileinfo = []
-        for root, dir, files in os.walk(".") :
-            for file in files :
-                if RegexHandler.is_pattern_match(file, pattern) :
-                    fileinfo.append(FileInfo(name = file, path = root))
-        return fileinfo
-        
 
-    def extract_image_links(self, fileinfo) :
-        def parse_link(sentence) :
-            split = RegexHandler.IMAGE_LINK.findall(sentence)[0]
-            ret = {
-                'alt' : split[0], 
-                'src' : split[1]
-            }
-            return ret
 
-        fp = open(fileinfo['directory'] + '/' + fileinfo['filename'], 'r')
-        links = []
-        while True :
-            sentence = fp.readline()
-            if not sentence : break
-            if RegexHandler.is_pattern_match(sentence, RegexHandler.IMAGE_LINK) :
-                links.append(parse_link(sentence))
-        fp.close()
-        return links
-
-    def is_alive(self, md, link) :
-        image = md['directory'] + '/' + link['src']
-        # print(image)
-        return os.path.isfile(image)
-        # print(os.path.isfile(image))
-        # print('md = ', md)
-        # print('link = ', link)
 
 if __name__ == '__main__' :
     print("Test of Marker starts ")
